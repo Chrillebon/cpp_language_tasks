b252a595fc19d18869b007cb9f9b407a18dec537
Chrillebon <christian.mikkelstrup@gmail.com>
2017-10-23 19:35:44 +0200

    ASolvedProblem

--- ADifferentProblem.cpp
+++ ADifferentProblem.cpp
@@ -3,15 +3,9 @@
 int main()
 {
   double A, B, D = 0;
-  std::cout.precision(15);
-  while(1)
+  std::cout.precision(25);
+  while(std::cin >> A >> B)
   {
-    char Input[33];
-    fgets(Input, 33, stdin);
-    if(sscanf(Input, "%lf %lf", &A, &B) != 2)
-    {
-      break;
-    }
     D = A-B;
     if (D<0)
     {
@@ -21,3 +15,20 @@ int main()
   }
   return 0;
 }
+
+/*int main()
+{
+  double D = 0;
+  std::cout.precision(15);
+  double A, B;
+  while(scanf("%lf %lf", &A,&B) != EOF);
+  {
+    D = A-B;
+    if (D<0)
+    {
+      D = D* (-1);
+    }
+    std::cout << D << '\n';
+  }
+  return 0;
+}*/
