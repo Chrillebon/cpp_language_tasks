d10a56bf9363382d38f4e964e7df2aa80b1a1381
Chrillebon <christian.mikkelstrup@gmail.com>
2017-10-26 13:44:33 +0200

    AAAAAAAAAAAAAARGH!!!

--- Uge 42 (1.5)/aaah.cpp
+++ Uge 42 (1.5)/aaah.cpp
@@ -6,14 +6,13 @@ int main()
 {
   string Today, Doctor;
   int LT, LD;
-  scanf("%s \n %s", &Today, &Doctor);
-  std::cout << Today << Doctor << '\n';
-  /*if(Today.find_first_of("h")>=Doctor.find_first_of("h"))
+  cin >> Today >> Doctor;
+  if(Today.find_first_of("h")>=Doctor.find_first_of("h"))
   {
-    std::cout << "go" << '\n';
+    cout << "go" << '\n';
   }
   else
   {
-    std::cout << "no" << '\n';
-  }*/
+    cout << "no" << '\n';
+  }
 }
