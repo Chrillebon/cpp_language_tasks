210d818233896d18082a412b33766d09b1d5882d
Chrillebon <christian.mikkelstrup@gmail.com>
2017-10-31 10:06:38 +0100

    I booked a room, time to sleep

--- Uge 43 (1.7)/bookingaroom.cpp
+++ Uge 43 (1.7)/bookingaroom.cpp
@@ -12,7 +12,7 @@ int main()
     std::cout << "too late" << '\n';
     return 0;
   }
-  std::vector<int> v;
+  std::vector<int> v(r+1);
   /*std::priority_queue<int, std::vector<int>, std::greater<int> > q;
   for(int i=0;i<n;i++)
   {
@@ -31,20 +31,18 @@ int main()
   for(int i=0;i<n;i++)
   {
     std::cin >> b;
-    v.push_back(b);
+    v[b]=b;
   }
-  std::sort(v.begin(),v.begin()+n-1);
   for(int i=r;i>0;i--)
   {
-    //std::cout << "update: " << n-1 <<" og "<< v[n-1] << " og " << &v[n-1] << '\n';
-    if(v[n-1]!=i)
+    //std::cout << "update: " << i <<" og "<< v[i] << " og " << &v[i] << '\n';
+    if(v[i]!=i)
     {
       //std::cout << "jeg goer noget!!!" << '\n';
       std::cout << i << '\n';
       return 0;
     }
-    n--;
-    v.erase((v.begin()+n));
+    v.erase((v.begin()+i));
     //std::cout << "Dette er hvad der er tilbage: " << n-1 << " og " << v[n-1] << " og " << i << '\n';
   }
   return 0;
