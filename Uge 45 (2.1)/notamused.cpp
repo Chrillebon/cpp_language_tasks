7f103ffbbda1e1f7039a6e28d61af008626f32af
Chrillebon <christian.mikkelstrup@gmail.com>
2017-11-06 23:32:51 +0100

    notamusedcausenotdone

---
+++ Uge 45 (2.1)/notamused.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+vector<string> Names;
+vector<int> Arrival(20);
+
+int sortname(string name)
+{
+  for(int i=0;i<20;i++)
+  if(strcmp(name,Names[i]))
+  {
+    
+  }
+}
+
+
+
+int main()
+{
+  string What;
+
+  while(std::cin >> What;)
+  {
+    if(What == "OPEN")
+    {
+
+    }
+    if(What == "ENTER")
+    if(What == "EXIT")
+    if(What == "CLOSE")
+  }
+  return 0;
+}
