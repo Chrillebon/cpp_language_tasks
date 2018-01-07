d19ca50bfbe4e34ecf22ee3d688b81d5aeb22d34
Chrillebon <christian.mikkelstrup@gmail.com>
2018-01-07 22:14:44 +0100

    working on part 2

--- adventofcode.cpp/7.cpp
+++ adventofcode.cpp/7.cpp
@@ -27,6 +27,19 @@ string move(Towers source)
   return buff;
 }
 
+int search(string Name, Towers* Tree)
+{
+  for(int i=0;i<1500;i++)
+  {
+    if(Tree[i].name() == Name)
+    {
+      return i;
+    }
+  }
+  std::cout << "Kan ikke finde den..." << '\n';
+  return -1;
+}
+
 int main()
 {
   /*Towers test;
@@ -51,6 +64,7 @@ int main()
     Programs[i].set(h);
     i++;
   }
+  /*
   int max = i-1;
   std::cout << "Antallet af programmer: " <<i-1<< '\n';
   //std::cout << "move test: " <<Programs[0].name()<< '\n';
@@ -94,6 +108,58 @@ int main()
       std::cout << "Og svaret er: " <<TOP[i].name()<< '\n';
       break;
     }
+  }*/
+  string Branch[50];
+  int BR [50]{};
+  int ID [50]{};
+  double Weights[8]{};
+  int dir=1;
+  int next;
+  int depth = 0;
+  Branch[depth] = "mkxke";
+  BR[depth] = 0;
+  ID[depth] = 1205;
+  while(depth >= 0)
+  {
+    std::cout << "Branch[1]" <<Branch[1]<< '\n';
+    if(Branch[1] == "uhcrfl") dir = 1;
+    else if(Branch[1] == "bnhfnlw") dir = 2;
+    else if(Branch[1] == "hdzls") dir = 3;
+    else if(Branch[1] == "pcfilur") dir = 4;
+    else if(Branch[1] == "onnfacs") dir = 5;
+    else if(Branch[1] == "wdugfj") dir = 6;
+    else if(Branch[1] == "jakdiea") dir = 7;
+    else std::cout << "'DIR' ERROR" << '\n';
+
+    std::cout << "Switch done: " <<dir<<" and "<<Programs[ID[depth]].relations()<< '\n';
+    if(Programs[ID[depth]].relations() - BR[depth] == 0)
+    {
+      Branch[depth] = "";
+      BR[depth] = 0;
+      ID[depth] = 0;
+      depth--;
+      BR[depth]++;
+    }
+    else
+    {
+      next = search(Programs[ID[depth]].relation(BR[depth]), Programs);
+      depth++;
+      Branch[depth] = Programs[next].name();
+      BR[depth] = 0;
+      ID[depth] = next;
+    }
+    Weights[dir] += Programs[ID[depth]].weight();
+    std::cout << "Weight: " <<Weights[dir]<<" name: "<<Branch[depth]<<" og depth: "<<depth<< '\n';
   }
+
+  std::cout << "Resultater af vægtene:" << '\n';
+  std::cout << "Nr. 1: " <<Weights[1]<< '\n';
+  std::cout << "Nr. 2: " <<Weights[2]<< '\n';
+  std::cout << "Nr. 3: " <<Weights[3]<< '\n';
+  std::cout << "Nr. 4: " <<Weights[4]<< '\n';
+  std::cout << "Nr. 5: " <<Weights[5]<< '\n';
+  std::cout << "Nr. 6: " <<Weights[6]<< '\n';
+  std::cout << "Nr. 7: " <<Weights[7]<< '\n';
+
   return 0;
 }
