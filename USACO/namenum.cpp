2d0d2048a99574cced4b3b0c9a5fb89266c871f8
Chrillebon <christian.mikkelstrup@gmail.com>
2018-03-20 22:47:31 +0100

    done for 2day, getting close to an awesome solution

--- USACO/namenum.cpp
+++ USACO/namenum.cpp
@@ -18,46 +18,74 @@ char Phone [8][3] = { {'A', 'B', 'C'}, {'D', 'E', 'F'},
                  {'M', 'N', 'O'}, {'P', 'R', 'S'},
                  {'T', 'U', 'V'}, {'W', 'X', 'Y'}};
 
+struct Node
+{
+  vector<string> posname;
+  Node *children[8] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+};
+
+void insert(Node * Parent, string name, int depth)
+{
+  if(depth == name.size())
+  {
+    Parent -> posname.push_back(name);
+    return;
+  }
+  int qcorrect = name[depth];
+  if(qcorrect >= 81)
+  {
+    qcorrect--;
+  }
+  int whereto = (qcorrect - 65) / 3;
+  if(Parent -> children[whereto] == NULL)
+  {
+    Parent -> children[whereto] = new Node;
+  }
+  return insert(Parent -> children[whereto], name, depth + 1);
+}
+
+void printval(Node * Parent, int * name, int depth, int length)
+{
+  std::cout << "how goes: " <<name[depth]<< '\n';
+  if(depth == length - 1)
+  {
+    std::cout << "the end" << '\n';
+    //Skriv alle navne
+    for(int i=0;i<Parent -> posname.size()-1;i++)
+    {
+      cout << Parent -> posname[i] << " ";
+    }
+    cout << Parent -> posname[Parent -> posname.size() - 1];
+    return;
+  }
+  //Eller bevæg dig ned ad næste
+  return printval(Parent -> children[name[depth]], name, depth + 1, length);
+}
+
 int main()
 {
-  ifstream dict;
-  dict.open("dict.txt");
+  ifstream dict ("dict.in");
+  ifstream fin ("namenum.in");
+  ofstream fout ("namenum.out");
+  Node *root = new Node;
+  string temp;
+  while(dict >> temp)
+  {
+    std::cout << temp << '\n';
+    insert(root, temp, 0);
+  }
   int N, length = 0;
-  std::cin >> N;
+  cin >> N;
   while(1.0*(N / pow(10, length)) >= 1)
   {
     length++;
   }
-  int all[length]{}, where = 0, dec;
-  char test[length];
+  int number[length]{};
   for(int i=0;i<length;i++)
   {
-    int power = pow(10,length-i-1);
-    test[i] = Phone[(N/power % 10)-2][0];
+    int power = pow(10,length-1-i);
+    number[i] = (N/power % 10)-2;
   }
-  while(all[length-1] < 3)
-  {
-    where = 0;
-    dec = N / pow(10, where);
-    test[where] = Phone[dec][all[where]];
-
-    all[where]++;
-    //Try all options
-    while(all[where] > 3)
-    {
-      all[where] = 0;
-      where++;
-      all[where]++;
-    }
-    //Find in file:
-    /*std::cout << getline(dict, "GREG") << '\n';
-    if(getline(dict, test) != -1)
-    {
-      std::cout << test << '\n';
-    }*/
-  }
-  string tst;
-  getline(dict, tst);
-  std::cout << tst.find("GREG",0) << '\n';
+  printval(root, number, 0, length);
   return 0;
 }
