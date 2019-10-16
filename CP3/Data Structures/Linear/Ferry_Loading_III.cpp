f695fa283ab74336e2016908c138a885d3701bff
Christian Mikkelstrup <chrillebon@ubuntu.com>
2019-10-16 14:14:54 -0700

    No more queueing today

--- CP3/Data Structures/Linear/Ferry_Loading_III.cpp
+++ CP3/Data Structures/Linear/Ferry_Loading_III.cpp
@@ -13,6 +13,10 @@ int main()
   std::cin >> c;
   for(int tc=0; tc<c;tc++)
   {
+    if(tc!=0)
+    {
+      std::cout << '\n';
+    }
     std::cin >> n >> t >> m;
     for(int car=0;car<m;car++)
     {
@@ -31,36 +35,49 @@ int main()
     int time=0;
     bool ferry=0; //0=left, 1=right;
     int load=0;
-    for(int car=0;car<m;car++)
+    int car = 0;
+    while(car<m)
     {
-      if(time < left.front().first or time < right.front().first)
+      if((time < left.front().first or left.empty()) and (time < right.front().first or right.empty()))
       {
-        time = std::min(left.front().first, right.front().first);
-      }
-      //venstre side
-      if(left.front().first < right.front().first or (right.empty() and !left.empty()) or (ferry == 0 && left.front().first == right.front().first))
-      {
-        load=0;
-        time += t*ferry; //tager t tid, hvis færgen er højre
-        while(left.front().first <= time && load < n) //hvis de er kommet og der er plads
+        if(left.empty())
         {
-            load++;
-            res[left.front().second] = time + t;
-            left.pop();
+          time = right.front().first;
+        }
+        else if(right.empty())
+        {
+          time = left.front().first;
+        }
+        else
+        {
+          time = std::min(left.front().first, right.front().first);
+        }
+      }
+      if(!ferry)      //venstre side
+      {
+        load = 0;
+        while(left.front().first <= time && load < n && !left.empty()) //hvis de er kommet og der er plads
+        {
+          load++;
+          res[left.front().second] = time + t;
+          left.pop();
+          car++;
         }
         time += t;
+        ferry ^= 1;
       }
-      if(right.front().first < left.front().first or (left.empty() and !right.empty()) or (ferry == 1 && left.front().first == right.front().first))
+      else      //højre side
       {
-        load=0;
-        time += t; time -= t*ferry; //tager t tid, hvis færgen er venstre
-        while(right.front().first <= time && load < n)
+        load = 0;
+        while(right.front().first <= time && load < n && !right.empty())
         {
           load++;
           res[right.front().second] = time + t;
           right.pop();
+          car++;
         }
         time += t;
+        ferry ^= 1;
       }
     }
     //skriv resultaterne:
@@ -68,7 +85,6 @@ int main()
     {
       std::cout << res[car] << '\n';
     }
-    std::cout << '\n';
   }
   return 0;
 }
