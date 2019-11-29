3f27b8ce6e2b13d68d2edf3a26b9690bfd425cfd
root <root@Chrillebon.II>
2019-11-29 07:51:42 +0100

    Keep cool, and do it later...

---
+++ Andet/NCPC/keep_it_cool.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> fridge;
+vector<int> sorted;
+vector<int> result;
+
+bool myfunction(int a, int b)
+{
+  return (fridge[a]<fridge[b]);
+}
+
+int n,m,s,d; //new bottles , students , slots , space
+
+int main()
+{
+  std::cin >> n >> m >> s >> d;
+  fridge.resize(s);
+  sorted.resize(s);
+  result.resize(s);
+  for(int i=0;i<s;i++) //indexer alle pladser
+  {
+    sorted[i] = i;
+  }
+  for(int i=0;i<s;i++)
+  {
+    std::cin >> fridge[i];
+  }
+  sort(sorted.begin(),sorted.end(), myfunction);//sorted giver id på mindste index af pladser
+  int space=0, filled = 0;
+  while(filled < n)
+  {
+    filled += d-fridge[sorted[space]];
+    result[sorted[space]] = d-fridge[sorted[space]];
+    space++;
+  }
+  result[sorted[space-1]] -= (filled - n); // trækker overindlæg fra
+  
+  for(int i=0;i<s;i++)
+  {
+    std::cout << result[i] << '\n';
+  }
+  return 0;
+}
